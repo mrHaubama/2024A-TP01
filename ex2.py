14fe1e4c8e3151b826d5564a5e36e334dadc5041
mrHaubama <martinesboetos@gmail.com>
2024-09-11 16:49:38 -0400

    exercice 2 fini

--- ex2.py
+++ ex2.py
@@ -2,5 +2,18 @@
 # TODO: Importer les modules nécessaires.
 
 
-water_quantity = 1+12
-water= 6
+water_quantity = int(input("Quelle quantité d'eau doit être assainit?"))
+
+def ressource_nécessaire(qt_eau):
+    multi= qt_eau/5
+    
+    nb_filtre= 1*multi
+    nb_lampsUV = 3*multi
+    nb_kgChlore = 0.5*multi
+    return nb_filtre, nb_lampsUV, nb_kgChlore
+
+def retour_message(x,y,z):
+    print("Voici les matériaux requis pour l'assainissement de 10L d'eau: \n\t - filtre: "+str(x)+"\n\t - lamp UV: "+str(y)+"\n\t - Chlore: "+str(z)+"kg") 
+
+filtre, lamp, chlore = ressource_nécessaire(water_quantity)
+retour_message(filtre, lamp, chlore)
