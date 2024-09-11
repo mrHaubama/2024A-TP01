001a8c5cc55102bb629b7bf8c704fc681b4c92a1
mrHaubama <martinesboetos@gmail.com>
2024-09-11 16:23:33 -0400

    first commit

--- ex2.py
+++ ex2.py
@@ -1,4 +1,6 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
 
-water_quantity = ...
+
+water_quantity = 1+12
+water= 6
