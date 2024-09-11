ece88d227b4e830c87404a1a90bb81e13b254116
mrHaubama <martinesboetos@gmail.com>
2024-09-11 17:28:11 -0400

    exe4 fini

--- ex4.py
+++ ex4.py
@@ -3,4 +3,27 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = ...
+battery_level = int(input("Le niveau de la batterie est: "))
+
+def distance_restant(entre):
+    distance = 0
+    bat= entre
+    while bat != 0:
+        if bat > 50:
+            bat -= 1
+            distance += 2
+        elif 50>= bat > 25:
+            bat -= 1
+            distance += 0.5
+        elif 25 >= bat >10:
+            bat -= 1
+            distance += 1 
+        elif 10 >= bat > 5:
+            bat -= 1
+            distance += 2.5
+        elif 5 >= bat > 0:
+            bat -= 1
+            distance += 6
+    return distance 
+
+print("Il reste ", distance_restant(battery_level), "km")
