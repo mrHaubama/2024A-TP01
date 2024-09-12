e3fb4c50dc58c6647408bde531b10e10ff8b36ec
HasanOtken1007 <hasotk04@gmail.com>
2024-09-11 21:53:12 -0400

    Ajout de 'm' dans derniere ligne code

--- ex3.py
+++ ex3.py
@@ -6,4 +6,4 @@ speed = float(input("Vitesse initiale (en ms/s): "))
 angle = float(input("Angle de lancement (en degres): "))
 distance =  (speed**2)*(math.sin(math.radians(2*angle))) / 9.8
 rounded_distance_max = round(distance, 2)
-print("la distance maximal en x est de",rounded_distance_max)
+print("la distance maximal en x est de",rounded_distance_max,"m")
