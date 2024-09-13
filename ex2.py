9af890ba07211a484996e03c99e1afea4916f2b7
mrHaubama <martinesboetos@gmail.com>
2024-09-13 11:59:53 -0400

    ex2 v3

--- ex2.py
+++ ex2.py
@@ -1,34 +1,35 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
 
-
 water_quantity = float(input("Quelle quantité d'eau faut-il assainir ? "))
 
-def ressource_nécessaire(qt_eau):
-    multi= qt_eau/5
-    
-    nb_filtre= 1*multi
-    
-    if nb_filtre%1 > 0:
-        nb_filtre = int(nb_filtre+1)
 
-    else:
-        nb_filtre =int(nb_filtre)
+multi= water_quantity/5
 
-    nb_lampsUV = 3*multi
-    
-    if nb_lampsUV %1 > 0:
-        nb_lampsUV = int(nb_lampsUV+1)
-    
-    else:
-        nb_lampsUV =int(nb_lampsUV)
-    
-    nb_kgChlore = 0.5*multi
-    
-    return nb_filtre, nb_lampsUV, nb_kgChlore
+nb_filtre= 1*multi
 
-def retour_message(x,y,z):
-    print("Voici les éléments requis pour assainir ", water_quantity,"L d'eau:\n\t- Filtre(s) : "+str(x)+"\n\t- Lampe(s) UV : "+str(y)+"\n\t- Chlore : "+str(z)+"kg\n") 
+if nb_filtre%1 > 0:
+    nb_filtre = int(nb_filtre+1)
 
-filtre, lamp, chlore = ressource_nécessaire(water_quantity)
-retour_message(filtre, lamp, chlore)
+else:
+    nb_filtre =int(nb_filtre)
+
+nb_lampsUV = 3*multi
+
+if nb_lampsUV %1 > 0:
+    nb_lampsUV = int(nb_lampsUV+1)
+
+else:
+    nb_lampsUV =int(nb_lampsUV)
+
+nb_kgChlore = 0.5*multi
+
+
+
+
+reponse = f"""Voici les éléments requis pour assainir {water_quantity}L d'eau:
+\t- Filtre(s) : {nb_filtre}
+\t- Lampe(s) UV : {nb_lampsUV}
+\t- Chlore : {nb_kgChlore}kg\n"""
+
+print(reponse)
